5a9d6fdcb23c1a0c4210dd2163b19029038adec1
Kumari Neha Rani <1.2ranineha456@gmail.com>
2025-03-31 09:33:16 +0530

    added top doctors component

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import Specialitymeanu from "../components/Specialitymeanu";
+import TopDoctors from "../components/TopDoctors";
 
 const home = () => {
     return(
         <div>
              <Header/>
              <Specialitymeanu/>
+             <TopDoctors/>
         </div>
     )
 }
