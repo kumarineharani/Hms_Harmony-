d13f896256c7d93ba5e79175ab98dff437098532
kumari neha rani <1.2ranineha456@gmail.com>
2025-04-21 17:15:01 +0530

    create related doctors footer for appointment page

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -25,7 +25,10 @@ const TopDoctors = () => {
         {doctors.slice(0, 10).map((item, index) => (
           <div
             key={index}
-            onClick={() => navigate(`/appointment/${item._id}`)}
+            onClick={() => {
+              navigate(`/appointment/${item._id}`);
+              window.scrollTo(0, 0);
+            }}
             className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[10px] transition-all duration-500'
           >
             <img className='bg-blue-50 ' src={item.image} alt=" " />
--- frontend/src/pages/appointment.jsx
+++ frontend/src/pages/appointment.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { AppContext } from '../context/AppContext';
 import { assets } from '../assets/assets_frontend/assets';
+import RealatedDoctors from '../components/RealatedDoctors';
 
 const Appointment = () => {
   const { docid } = useParams(); // Get doctor ID from route
@@ -35,11 +36,11 @@ const Appointment = () => {
       if (today.getDate() === currentDate.getDate()) {
         const hour = currentDate.getHours();
         const minute = currentDate.getMinutes();
-      
+
         currentDate.setHours(hour >= 10 ? hour + (minute >= 30 ? 1 : 0) : 10);
         currentDate.setMinutes(minute >= 30 ? 0 : 30);
       }
-      
+
       else {
         currentDate.setHours(10)
         currentDate.setMinutes(0)
@@ -112,7 +113,7 @@ const Appointment = () => {
         <div className='flex gap-3 items-center w-full overflow-x-scroll mt-4'>
           {
             docSlots.length && docSlots.map((item, index) => (
-              <div onClick={()=> setSlotIndex(index)}
+              <div onClick={() => setSlotIndex(index)}
                 className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex === index ? 'bg-blue-500 text-white' : 'border border-gray-200'
                   }`}
                 key={index}
@@ -125,18 +126,25 @@ const Appointment = () => {
           }
         </div>
         <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
-        {docSlots.length && docSlots[slotIndex].map((item, index) => (
-  <p on onClick={()=>setSlotTime(item.time)}
-    className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-blue-500 text-white' : 'text-gray-400 border border-gray-300'}`}
-    key={index}
-  >
-    {item.time.toLowerCase()}
-  </p>
-))}
+          {docSlots.length && docSlots[slotIndex].map((item, index) => (
+            <p on onClick={() => setSlotTime(item.time)}
+              className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-blue-500 text-white' : 'text-gray-400 border border-gray-300'}`}
+              key={index}
+            >
+              {item.time.toLowerCase()}
+            </p>
+          ))}
 
-</div>
+        </div>
+
+        <button className="bg-blue-500 hover:bg-blue-600 text-blue-600 px-6 py-3 rounded-full">
+  Book an Appointment
+</button>
+
 
       </div>
+      {/*----------listing reated doctors -*/}
+      <RealatedDoctors docid={docid} speciality={docInfo.speciality}/>
     </div>
   );
 }
